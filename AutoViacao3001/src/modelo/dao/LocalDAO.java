8e2e2977bb7270633267af74ec322c30c7c0ae4d
Felipe <felipe.reis@uniriotec.br>
2015-10-31 17:15:39 -0200

    Comittando arquivo que esqueci ontem.

--- AutoViacao3001/src/modelo/dao/LocalDAO.java
+++ AutoViacao3001/src/modelo/dao/LocalDAO.java
@@ -3,11 +3,13 @@ package modelo.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import modelo.entidade.Local;
 
 public class LocalDAO extends BaseCrudDAO<Local> {
-
+	
 	private final String tabelaLocal = "local";
 
 	public LocalDAO(ConexaoDAO conexao) {
@@ -40,38 +42,58 @@ public class LocalDAO extends BaseCrudDAO<Local> {
 
 	@Override
 	public String getQueryDeListar() {
-		return "SELECT * FROM " + tabelaLocal;
-	}
-
-	@Override
-	public String getQueryDeBusca(Object identificador) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	public String getQueryDeBusca(Object identificador) {
+		return "SELECT * FROM " + tabelaLocal + " WHERE ID = " + identificador; 
+	}
+
+	@Override
 	public Local getEntidade(ResultSet registro) {
-		Local local = null;
-		try {
-			local = new Local();
-			local.setId(registro.getInt("id_local"));
-			local.setNome(registro.getString("nome"));
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return local;
+		Local local;
+        try {
+        	local = new Local();
+        	local.setId(registro.getInt("id"));
+        	local.setNome(registro.getString("nome"));
+        	return local;
+        } catch (SQLException ex) {
+        	System.out.println("Erro ao pegar entidade no banco - " + ex);
+            Logger.getLogger(PoltronaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
 	}
 
 	@Override
 	public void incluirDadosNoBanco(PreparedStatement pst, Local entidade) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
-	public Local metodoDeBusca(ResultSet registro, Local entidade) {
-		// TODO Auto-generated method stub
-		return null;
+	public Local metodoDeBusca(ResultSet registro, Local local) {
+		try {
+			while (registro.next()) {
+				local = getEntidade(registro);
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro no método de busca: " + e.getMessage());
+			e.printStackTrace();
+		}
+		return local;
+	}
+	
+	public String getNomeLocal(Integer id){
+		String nome = "";
+		try {
+			nome = buscar(id).getNome();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return nome;
 	}
 
+	
 }
